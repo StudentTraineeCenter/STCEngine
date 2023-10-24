e9ea99f396e8800d87fdde151f1e10d46c4bf2c4
Admexir <ajazak293@gmail.com>
2023-10-24 16:56:31 +0200

    Update loops, gameobject objects and basic window

--- Test Apka STC Engine/Engine.cs
+++ Test Apka STC Engine/Engine.cs
@@ -1,35 +1,97 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.Threading;
 //using System.Windows;
 //using System.Drawing;
 //using System.Numerics;
 
 
-namespace Test_Apka_STC_Engine.Engine
+namespace STCEngine.Engine
+{
+    public abstract class Engine
+    {
+        private Vector2 screenSize;
+        private string title;
+        private Canvas window;
+        public Thread gameLoopThread;
+
+        public static List<GameObject> gameObjects = new List<GameObject>();
+        public Color backgroundColor;
+        public Engine(Vector2 screenSize, string title)
+        {
+            this.screenSize = screenSize;
+            this.title = title;
+            window = new Canvas();
+            window.Size = new Size((int)this.screenSize.x, (int)this.screenSize.y);
+            window.Text = this.title;
+            window.Paint += Renderer;
+
+            OnLoad();
+            gameLoopThread = new Thread(GameLoop);
+            gameLoopThread.Start();
+            
+            Application.Run(window);
+
+            OnExit();
+        }
+        private void GameLoop()
+        {
+            while (gameLoopThread.IsAlive)
+            {
+                try
+                {
+                    Update();
+                    window.BeginInvoke((MethodInvoker)delegate { window.Refresh(); });
+                    LateUpdate();
+                
+                    Thread.Sleep(1);
+                }
+                catch
+                {
+                    Console.WriteLine("Loading...");
+                }
+            }
+        }
+        private void Renderer(object sender, PaintEventArgs args)
+        {
+            Graphics graphics = args.Graphics;
+            graphics.Clear(backgroundColor);
+        }
+        public abstract void Update();
+        public abstract void LateUpdate();
+        public abstract void OnLoad();
+        public abstract void OnExit();
+        public static void AddGameObject(GameObject gameObject) { gameObjects.Add(gameObject); }
+    }
+
+    public class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+
+
+}
+namespace STCEngine
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-            var a = new Vector2();
-            a.x = 1;
-            a.y = 23;
-            var b = new Vector2(454, 312);
-            Console.WriteLine("a == (" + a.x + ", " + a.y + "), length: " + a.length);
-            Console.WriteLine("b == (" + b.x + ", " + b.y + ")");
+            Game testGame = new Game();
+            //var a = new Vector2();
+            //a.x = 1;
+            //a.y = 23;
+            //var b = new Vector2(454, 312);
+            //Console.WriteLine("a == (" + a.x + ", " + a.y + "), length: " + a.length);
+            //Console.WriteLine("b == (" + b.x + ", " + b.y + ")");
         }
     }
- 
 
-    public abstract class Engine
-    {
-        private Vector2 screenSize;
-        private string title;
-        //private 
-    }
-
-
-    class Vector2
+    public class Vector2
     {
         public float x { get; set; }
         public float y { get; set; }
@@ -53,18 +115,27 @@ namespace Test_Apka_STC_Engine.Engine
         }
 
     }
-    class GameObject
+    public class GameObject
     {
         public Vector2 position;
         public float rotation;
         public Vector2 size;
 
+        public GameObject(Vector2 position = null, float rotation = 0, Vector2 size = null)
+        {
+            this.position = position == null ? Vector2.zero : position;
+            this.rotation = rotation;
+            this.size = size == null ? Vector2.zero : size;
+        }
         //public 
         //public GameObject() { }
     }
-
-    class Sprite : GameObject
+    public class Sprite : GameObject
     {
-
+        public Image image;
+        public Sprite(Image image)
+        {
+            this.image = image;
+        }
     }
 }
--- Test Apka STC Engine/Game.cs
+++ Test Apka STC Engine/Game.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using STCEngine.Engine;
 
-namespace Test_Apka_STC_Engine.Game
+namespace STCEngine
 {
-    internal class Game
+    class Game : Engine.Engine
     {
+        public Game() : base(new Vector2(512, 512), "Hraaa :)") { } 
+        public override void OnLoad() 
+        {
+            Console.WriteLine("game started");
+            backgroundColor = Color.Black;
+        }
+        public override void OnExit()
+        {
+            gameLoopThread.Interrupt();
+            Console.WriteLine("game quit");
+        }
+
+        int frame = 0;
+        public override void Update()
+        {
+            Console.WriteLine($"Update {frame}");
+            frame += 1;
+        }
+
+        public override void LateUpdate()
+        {
+            //Console.WriteLine("LateUpdate");
+        }
     }
 }
